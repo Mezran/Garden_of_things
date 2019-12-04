a18252d7c95f9442a9096afaa09ee2346ec9269d
Stephen Allen <staylorallen@gmail.com>
2019-12-04 12:46:29 -0500

    Updates

---
+++ precache-manifest.5ea7a9a80e0de52c9f7416df27ebaaec.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835dd7ef51af20a83d34550ebbe9a9bf",
+    "url": "/Garden_of_things/index.html"
+  },
+  {
+    "revision": "a59fa443acd20c1d9452",
+    "url": "/Garden_of_things/static/css/main.496130fe.chunk.css"
+  },
+  {
+    "revision": "94e4d78030404889892b",
+    "url": "/Garden_of_things/static/js/2.60d0153d.chunk.js"
+  },
+  {
+    "revision": "a59fa443acd20c1d9452",
+    "url": "/Garden_of_things/static/js/main.191e407e.chunk.js"
+  },
+  {
+    "revision": "b5f22df660687d85389d",
+    "url": "/Garden_of_things/static/js/runtime-main.19b62f8b.js"
+  },
+  {
+    "revision": "0d4a0955d995846aa716357ef66cbd40",
+    "url": "/Garden_of_things/static/media/GardenOfThings.0d4a0955.png"
+  },
+  {
+    "revision": "23dd21ec41404d4d94919a41629261b3",
+    "url": "/Garden_of_things/static/media/fertilizer.23dd21ec.png"
+  },
+  {
+    "revision": "100e6e59949d07e11448342e6f48a093",
+    "url": "/Garden_of_things/static/media/garden0.100e6e59.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Garden_of_things/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "e145e98820ef1b1dd1481e41f8fea91e",
+    "url": "/Garden_of_things/static/media/sun.e145e988.png"
+  },
+  {
+    "revision": "aef2215c80d15e38b12870bce30d83be",
+    "url": "/Garden_of_things/static/media/water-drop.aef2215c.png"
+  }
+]);
